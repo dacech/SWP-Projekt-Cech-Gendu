24f2417126655cf987774bc2d199218ec4f2d834
dacech <dacech44@gmail.com>
2022-05-04 09:36:05 +0200

    DB-Erweiterung mit sql File, und UserDB-Folder

--- src/Hauptprogramm.java
+++ src/Hauptprogramm.java
@@ -1,7 +1,51 @@
+import java.sql.SQLException;
+import java.util.*;
+import UserDB.*;
 
 public class Hauptprogramm {
 
     public static void main(String[] args) {
-        System.out.println("Noch nicht programmiert!");
+
+        //DB Verknüpfung
+        IRepositoryUser repU = null;
+
+        /*
+        User u1 = new User(1, "Johny", "password1");
+        if(repU.insertUser(u1)){
+            System.out.println("Johny  wurde hinzugefügt!");
+        }
+        else{
+            System.out.println("Johny konnte nicht hinzugefügt werden!");
+        }
+         */
+
+
+        try {
+
+            repU = new RepositoryUserDB();
+            repU.open();
+
+
+
+
+
+        }
+
+        catch(ClassNotFoundException e){
+            System.out.println("Der DB-Treiber konnte nicht geladen werden!");
+        }
+        catch(SQLException e){
+            System.out.println("Datenbankfehler!");
+            System.out.println(e.getMessage());
+        }
+        finally{
+            try {
+                repU.close();
+
+            }
+            catch(SQLException e){
+                System.out.println("Die Verbindung konnte nicht geschlossen werden!");
+            }
+        }
     }
 }

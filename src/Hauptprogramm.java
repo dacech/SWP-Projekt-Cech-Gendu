f0c29d1ce58f1c71f0307233a2f9a2d3e6101acb
dacech <dacech44@gmail.com>
2022-05-06 12:30:32 +0200

    Funktionirende HPG bis ende Login mit allen Funktionen

--- src/Hauptprogramm.java
+++ src/Hauptprogramm.java
@@ -2,9 +2,14 @@ import java.sql.SQLException;
 import java.util.*;
 import UserDB.*;
 
+import javax.swing.*;
+
 public class Hauptprogramm {
 
+    private static Scanner reader = new Scanner(System.in);
+
     public static void main(String[] args) {
+
 
         //DB Verknüpfung
         IRepositoryUser repU = null;
@@ -24,6 +29,129 @@ public class Hauptprogramm {
 
             repU = new RepositoryUserDB();
             repU.open();
+
+
+            //start
+            Scanner reader1 = new Scanner(System.in);
+            String username, password;
+            String loggedInUser = null;
+
+            char choiseStartMenu;
+            char choiseLoggedInMenu;
+
+            System.out.print("\nWilkommen im Vokabeltrainer!\n");
+
+            do {
+                choiseStartMenu = startMenu();
+                switch (choiseStartMenu) {
+
+                    case 'l':
+
+                        System.out.println("Benutzername:");
+                        username = reader1.nextLine();
+
+                        System.out.println("Password:");
+                        password = reader1.nextLine();
+
+                      /*
+                    for (user user : listOfUsers) {
+                        if (user.getUsername().equals(username)) {
+                            if (user.getPassword().equals(password)) {
+                                loggedInUser = user;
+                                //break stoppt die suche wenn der passende user gefunden worden ist
+                                break;
+                            }
+                        }
+                    }
+                    // wenn loggedInUser verändert wird , war der login erfolgreich
+                    if (loggedInUser != null) {
+                     */
+
+                        if (repU.checkLoginData(username,password)) {
+                            loggedInUser = username;
+                            System.out.println("Login erfolgreich: "+ username);
+
+                            do {
+                                choiseLoggedInMenu = loggedInMenu();
+                                switch (choiseLoggedInMenu) {
+                                    case 'v':
+
+                                            //Noch nicht programmiert
+                                        System.out.println("NOCH NICHT PROGRAMMIERT!!");
+                                            break;
+
+
+                                    case 'l':
+                                        loggedInUser = null;
+                                        System.out.println("Sie werden abgemeldet!");
+                                        break;
+                                    default:
+                                        System.out.println("Sie haben eine falsche Taste gedrückt!");
+                                        break;
+                                }
+                            } while (choiseLoggedInMenu != 'l');
+
+                        } else {
+                            System.out.println("Ungültiger Benutzenname und/oder Password!");
+                        }
+
+                        break;
+
+                    case 'n':
+
+
+                        if (repU.insertUser(inputUser())) {
+                            System.out.println("Neuer User hinzugefügt!");
+                        } else {
+                            System.out.println("Neuer User konnte nicht gespeichert werden.");
+                        }
+                        break;
+
+
+                    case 'd':
+                        System.out.print("Geben Sie den Benutzernamen des zu löschenden Users ein: ");
+                        String username1 = reader.next();
+
+                        if (repU.removeUser(username1)) {
+                            System.out.println("User wurde dem Adressbuch gelöscht!");
+                        } else {
+                            System.out.println("User konnte nicht gelöscht werden!");
+                        }
+
+                        break;
+
+
+                    case 'b':
+                        System.out.print("Geben Sie den Benutzernamen des zu ändernden Users ein: ");
+                        String oldUsername = reader1.nextLine();
+
+                        System.out.println("Geben Sie bitte Ihren neuen Benutzernamen ein:");
+                        String newUsername = reader1.nextLine();
+
+                        System.out.println("Geben Sie bitte Ihr neues Passwort ein:");
+                        String newPassword = reader1.nextLine();
+                        // Userdaten ändern
+
+                        if (repU.changeUserData(oldUsername,newUsername,newPassword)) {
+                            System.out.println("Die Daten wurden erfolgreich geändert!");
+                        } else {
+                            System.out.println("Die Daten des Users konnten nicht geändert werden.");
+                        }
+                        break;
+
+                    case 'x':
+                        System.out.println("\nProgramm wird beendet!");
+                        break;
+                    case 'm':
+                        System.out.println("\nEasteregg entdeckt!");
+                        secret();
+                        break;
+
+                    default:
+                        System.out.println("Sie haben eine falsche Taste gedrückt!");
+                        break;
+                }
+            } while (choiseStartMenu != 'x');
 
 
 
@@ -47,5 +175,48 @@ public class Hauptprogramm {
                 System.out.println("Die Verbindung konnte nicht geschlossen werden!");
             }
         }
+    }
+    public static char startMenu() {
+        System.out.println("\nStartmenü:");
+        System.out.println("l ... login");
+        System.out.println("n ... neuen Benutzer anlegen");
+        System.out.println("d ... Benutzer löschen");
+        System.out.println("b ... Benutzer bearbeiten");
+        System.out.println("x ... Programm beenden");
+        System.out.print("Ihre Wahl: ");
+        return reader.next().toLowerCase().charAt(0);
+    }
+    public static void secret()  {
+        JFrame monke_secret = new JFrame("MONKE SECRET");
+        monke_secret.setSize(666,730);
+        monke_secret.setAlwaysOnTop(true);
+        monke_secret.setResizable(false);
+        monke_secret.setLocationRelativeTo(null);
+
+
+        ImageIcon icon = new ImageIcon ("C:\\Users\\David Cech\\Desktop\\HTL\\1BHWII\\SWP1\\Anderes\\monke.jpg");
+        JLabel l1 = new JLabel (icon);
+        monke_secret.add(l1); // Label hinzufügen
+
+        monke_secret.setVisible(true);
+    }
+    public static User inputUser() {
+
+        User u = new User();
+
+        // Contact-Daten eingeben lassen
+        System.out.print("\nBenutzername: ");
+        u.setUsername(reader.next());
+        System.out.print("\nPasswort: ");
+        u.setPassword(reader.next());
+
+        return u;
+    }
+    public static char loggedInMenu() {
+        System.out.println("\nDienste:");
+        System.out.println("v ... Vokabeltrainer");
+        System.out.println("l ... logout");
+        System.out.print("Ihre Wahl: ");
+        return reader.next().toLowerCase().charAt(0);
     }
 }

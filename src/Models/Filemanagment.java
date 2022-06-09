83dc6f3d5ee3c294bd517a90b0c6d8bcb22a92b0
dacech <dacech44@gmail.com>
2022-06-09 19:09:21 +0200

    Hashmap Model und Testordner --

--- src/Models/Filemanagment.java
+++ src/Models/Filemanagment.java
@@ -12,103 +12,97 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-<<<<<<< HEAD:src/Models/Filemanagment.java
+
 public class Filemanagment {
     static String path = "C:\\Users\\Jonat\\OneDrive\\Desktop\\Schule\\3Klasse\\SWP\\repository\\SWP-Projekt-Cech-Gendu\\Vocabs_Egger_Daniel_202122 (1).csv";
-=======
-public class Filemanagement_test {
-    static String path = "C:\\Users\\David Cech\\Desktop\\HTL\\1BHWII\\SWP1\\repositories\\SWP-Projekt-Cech-Gendu\\Vocabs_Egger_Daniel_202122 (1).csv";
->>>>>>> 123898d3112f926cb2587a0e64b752583241182a:src/Test/Filemanagement_test.java
 
-    public static void main(String[] args) {
+    public static class Filemanagement_test {
+        static String path = "C:\\Users\\David Cech\\Desktop\\HTL\\1BHWII\\SWP1\\repositories\\SWP-Projekt-Cech-Gendu\\Vocabs_Egger_Daniel_202122 (1).csv";
 
 
-        //System.out.println(ReadFile(path));
-        HashMap<String,String> hash = VocabBasicToHashMap(ReadFile(path));
-
-        for(String value : hash.values()){
-            System.out.println(value);
-        }
-
-    }
-
-    public static List<Vocab_Basic> ReadFile(String p){
-        Path path = Path.of(p);
-        List<String> row = new ArrayList<String>();
-        List<Vocab_Basic> Vocabs = new ArrayList<Vocab_Basic>();
-
-        try {
-            row = Files.readAllLines(path);
-            for (String rows : row){
-                if(rows.isEmpty()){
-                    continue;
-                }
-
-                //System.out.println(rows);
-                String[] r = new String[2];
-                r = rows.split("-");
-
-                //System.out.println(r[0]);
-                //System.out.println(r[1]);
-
-                Vocab_Basic v = new Vocab_Basic();
+        public static void main(String[] args) {
 
 
-                v.set_vocab_eng(r[0]);
-                v.set_vocab_ger(r[1]);
+            //System.out.println(ReadFile(path));
+            HashMap<String, String> hash = VocabBasicToHashMap(ReadFile(path));
 
-
-                v.set_vocab_dif(Difficulty.easy);
-                v.set_vocab_type(Type.basic);
-                //System.out.println(v);
-                Vocabs.add(v);
-
+            for (String value : hash.values()) {
+                System.out.println(value);
             }
 
+        }
+
+        public static List<Vocab_Basic> ReadFile(String p) {
+            Path path = Path.of(p);
+            List<String> row = new ArrayList<String>();
+            List<Vocab_Basic> Vocabs = new ArrayList<Vocab_Basic>();
+
+            try {
+                row = Files.readAllLines(path);
+                for (String rows : row) {
+                    if (rows.isEmpty()) {
+                        continue;
+                    }
+
+                    //System.out.println(rows);
+                    String[] r = new String[2];
+                    r = rows.split("-");
+
+                    //System.out.println(r[0]);
+                    //System.out.println(r[1]);
+
+                    Vocab_Basic v = new Vocab_Basic();
+
+
+                    v.set_vocab_eng(r[0]);
+                    v.set_vocab_ger(r[1]);
+
+
+                    v.set_vocab_dif(Difficulty.easy);
+                    v.set_vocab_type(Type.basic);
+                    //System.out.println(v);
+                    Vocabs.add(v);
+
+                }
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Vocabs);
+            return Vocabs;
+        }
+
+        // public static Vocab_Basic ArrayToVocab_basic (String[] r){
+
+
+        // }
+
+
+        public static HashMap<String, String> VocabToHashMap(List<Vocab_Basic> Vocab) {
+            return null;
+        }
+
+        public static HashMap<String, String> VocabBasicToHashMap(List<Vocab_Basic> Vocab) {
+
+            HashMap<String, String> h = new HashMap<String, String>();
+
+            for (Vocab_Basic v : Vocab) {
+
+                h.put(v.get_vocab_eng(), v.get_vocab_ger());
+
+
+            }
+            return h;
 
         }
-        catch (IOException e) {
-            e.printStackTrace();
+
+        public static Vocab HashMapToVocab(HashMap<String, String> Hash) {
+            Vocab v = new Vocab();
+            return v;
         }
-        System.out.println(Vocabs);
-        return Vocabs;
-    }
 
-    // public static Vocab_Basic ArrayToVocab_basic (String[] r){
-
-
-
-        
-   // }
-
-
-    public static HashMap<String,String> VocabToHashMap (List<Vocab_Basic> Vocab) {
-        return null;
-    }
-
-    public static HashMap<String,String> VocabBasicToHashMap (List<Vocab_Basic> Vocab){
-
-        HashMap<String,String> h = new HashMap<String,String>();
-
-        for (Vocab_Basic v : Vocab){
-
-            h.put(v.get_vocab_eng(),v.get_vocab_ger());
-
-
-        }
-        return h;
 
     }
-
-    public static Vocab HashMapToVocab (HashMap<String,String> Hash){
-        Vocab v = new Vocab();
-        return v;
-    }
-
-
-
-
-
-
 }
 
--- src/Models/Hashmap.java
+++ src/Models/Hashmap.java
@@ -1,4 +1,52 @@
 package Models;
 
+import Enums.Difficulty;
+import Enums.Type;
+import Vocab.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Hashmap {
+    // erweiterung & automatisierung
+    public static void main(String[] args) {
+
+        List<Vocab_Basic> vocabs = new ArrayList<Vocab_Basic>();
+
+        Vocab_Basic v1 = new Vocab_Basic(1, Difficulty.easy, Type.basic, "Baum", "tree");
+        Vocab_Basic v2 = new Vocab_Basic(2, Difficulty.easy, Type.basic, "Haus", "house");
+        Vocab_Basic v3 = new Vocab_Basic(3, Difficulty.easy, Type.basic, "Rechner", "calculator");
+        Vocab_Basic v4 = new Vocab_Basic(4, Difficulty.easy, Type.basic, "Tisch", "desk");
+        Vocab_Basic v5 = new Vocab_Basic(5, Difficulty.easy, Type.basic, "Maus", "mouse");
+
+        vocabs.add(v1);
+        vocabs.add(v2);
+        vocabs.add(v3);
+        vocabs.add(v4);
+        vocabs.add(v5);
+
+        HashMap<String, String> transger = new HashMap();
+        HashMap<String, String> transeng = new HashMap();
+
+        transger.put(v1.get_vocab_ger(), v1.get_vocab_eng());
+        transeng.put(v1.get_vocab_eng(),v1.get_vocab_ger());
+
+        System.out.println(v1.get_vocab_ger()+ " | " + transger.get(v1.get_vocab_ger()));
+        System.out.println(v1.get_vocab_eng() + " | " + transeng.get(v1.get_vocab_eng()));
+
+        /*
+        int counter = 0;
+
+        for (Vocab_Basic v_b : vocabs) {
+            counter++;
+        }
+        */
+
+
+
+    }
+
 }
+
